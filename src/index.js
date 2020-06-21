554948e5bf2a5bb1dd202b9ae0f2f86e85b3c6d4
HughG-50 <hugh.greethead@gmail.com>
2020-06-22 09:52:48 +1000

    Board has state

--- src/index.js
+++ src/index.js
@@ -3,23 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props){
-        //In JS classes, you need to call super when defining the constructor of a subclass
-        //All React component classes that have a constructor should start with a super(props) call
-        super(props)
-        this.state = {
-            value: null
-        }
-    }
-    
-    
     render() {
       return (
         <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -33,9 +23,19 @@ class Square extends React.Component {
         }
     }
     
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({squares: squares})
+    }
     
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}/>;
+      return( 
+        <Square 
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+      )
     }
   
     render() {
